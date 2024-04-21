765d3d7cc8567b42839767799dd237cecb8b8ce8
CosmicSurgery <mileskeating@gmail.com>
2024-04-21 14:15:17 +0200

    trying github c mnist scripts

--- prototype/C_snippets.c
+++ prototype/C_snippets.c
@@ -1,12 +1,27 @@
 
 #include <stdio.h>
+// #include "utils.h"
+#define USE_MNIST_LOADER
+#define MNIST_DOUBLE
+#include "mnist.h"
 
-void snippet(){
-    printf("This is a test");
+int snippet(){
+        mnist_data *data;
+        unsigned int cnt;
+        int ret;
+
+        if (ret = mnist_load(".\\data\\MNIST\\raw\\train-images-idx3-ubyte", ".\\data\\MNIST\\raw\\train-labels-idx1-ubyte", &data, &cnt)) {
+                printf("An error occured: %d\n", ret);
+        } else {
+                printf("image count: %d\n", cnt);
+        
+                free(data);
+        }
+
+        return 0;
 }
 
 void main(){
-    snippet();
+    printf("%d",snippet());
 }
-
 
--- prototype/utils.h
+++ prototype/utils.h
@@ -3,3 +3,10 @@
 #ifndef utils_h
 #define utils_h
 
+const char* snippet(){
+    return "This is a test";
+}
+
+
+#endif
+
