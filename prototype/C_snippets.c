e4d05fec04dc759d5291d76b98ec7a8fcaa87fa0
CosmicSurgery <mileskeating@gmail.com>
2024-04-25 22:19:36 +0200

    Reading lines from .txt

--- prototype/C_snippets.c
+++ prototype/C_snippets.c
@@ -1,16 +1,26 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include<string.h>
 
 
 int snippet(){
-
         char train_file[] = "MNIST_train.txt";
         char test_file[] = "MNIST_test.txt";
-        // FILE *fp;
+        FILE *file = fopen(test_file, "r");
+        if (file == NULL) {
+                printf("Error opening file\n");
+                return 1;
+        }
 
-        FILE *fp = fopen(test_file, "r");
-        fclose(fp);
+        char line[1000];
+        int count = 0;
+        while (fgets(line, sizeof(line), file) != NULL && count < 5) {
+                printf("%s", line);
+                count++;
+        }
+
+        fclose(file);
         return 0;
 }
 
