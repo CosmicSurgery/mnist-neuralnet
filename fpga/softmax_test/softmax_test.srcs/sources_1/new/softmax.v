54747eb324481286e13b75c06a70ab0bb259e44f
CosmicSurgery <mileskeating@gmail.com>
2024-06-11 15:02:37 +0200

    experimenting with softmax

---
+++ fpga/softmax_test/softmax_test.srcs/sources_1/new/softmax.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/10/2024 08:54:29 PM
+// Design Name: 
+// Module Name: softmax
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module softmax(
+    input clk,
+    input rst,
+    input [31:0]z,
+    input zValid,
+    output loss,
+    output lossValid
+    );
+    
+    
+    
+    
+    
+endmodule
