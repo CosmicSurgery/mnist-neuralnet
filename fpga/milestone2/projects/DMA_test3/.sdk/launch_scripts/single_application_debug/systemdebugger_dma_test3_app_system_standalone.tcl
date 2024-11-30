6f080d17f4418105a4a5b0237a2aeefd4535f17b
CosmicSurgery <mileskeating@gmail.com>
2024-11-29 21:02:02 -0800

    validated reading from DDR and writing to BRAM

---
+++ fpga/milestone2/projects/DMA_test3/.sdk/launch_scripts/single_application_debug/systemdebugger_dma_test3_app_system_standalone.tcl
@@ -0,0 +1,17 @@
+connect -url tcp:127.0.0.1:3121
+targets -set -nocase -filter {name =~"APU*"}
+rst -system
+after 3000
+targets -set -filter {jtag_cable_name =~ "Digilent Zybo 210279787858A" && level==0} -index 1
+fpga -file C:/git_repos/mnist_neuralnet/fpga/milestone2/projects/DMA_test3/DMA_test3_app/_ide/bitstream/design_1_wrapper.bit
+targets -set -nocase -filter {name =~"APU*"}
+loadhw -hw C:/git_repos/mnist_neuralnet/fpga/milestone2/projects/DMA_test3/DMA_test3/export/DMA_test3/hw/design_1_wrapper.xsa -mem-ranges [list {0x40000000 0xbfffffff}]
+configparams force-mem-access 1
+targets -set -nocase -filter {name =~"APU*"}
+source C:/git_repos/mnist_neuralnet/fpga/milestone2/projects/DMA_test3/DMA_test3_app/_ide/psinit/ps7_init.tcl
+ps7_init
+ps7_post_config
+targets -set -nocase -filter {name =~ "*A9*#0"}
+dow C:/git_repos/mnist_neuralnet/fpga/milestone2/projects/DMA_test3/DMA_test3_app/Debug/DMA_test3_app.elf
+configparams force-mem-access 0
+bpadd -addr &main
